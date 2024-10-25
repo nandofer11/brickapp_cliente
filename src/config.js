7ce2305fff2c2cf46fe1e2ffaf35e69054e72697
Orlando Fernández <nandofer.11@hotmail.com>
2024-10-25 15:56:28 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3002/api/admin/'
-    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://brickapp-server.onrender.com/'
+    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://brickapp-server.onrender.com/api/admin/'
   };
   
   export default config;
